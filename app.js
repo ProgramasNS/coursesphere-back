57d77a0e6b0ab05c006db80ceb205f029413ff89
ProgramasNS <programasns@gmail.com>
2026-05-10 15:35:06 -0300

    Adição da integração front-back

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ import cors from 'cors';
 
 //Criação do app e uso dos módulos
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json())
 
 
